22a3ee3c04353dc83a04f6598024e3d5a6c0005d
eslamheshamm <esl2mhesham@gmail.com>
2021-06-14 21:22:04 +0200

    building pages

---
+++ src/pages/info.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layouts from "../components/layouts";
+
+import SectionThree from "../components/home/sectionThree";
+
+const InfoPage = () => {
+	return (
+		<Layouts>
+			<SectionThree />
+		</Layouts>
+	);
+};
+
+export default InfoPage;
