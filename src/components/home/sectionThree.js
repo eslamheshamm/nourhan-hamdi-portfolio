9b8567bfc4e30cf5750c8d1665696fd0fb270a64
eslamheshamm <esl2mhesham@gmail.com>
2021-06-14 21:19:17 +0200

    building slider section

---
+++ src/components/home/sectionThree.js
@@ -0,0 +1,69 @@
+import React from "react";
+const SectionThree = () => {
+	return (
+		<section className="min-h-screen bg-[#f3f1ed] py-24 pb-32">
+			<h3 className="text-[#FF1D00] uppercase  font-bold text-center tracking-wider mb-10">
+				About me
+			</h3>
+			<article className="grid grid-cols-2 gap-6 my-4">
+				<h2 className=" font-Guyot text-5xl italic text-center">
+					Hi my Name Is Nourhan Hamdii, <br /> Photography is my hobby.
+				</h2>
+				<ul className="list-decimal  text-[22px] text-[#949494]  font-Freight px-8 ">
+					<li>
+						Art is a play with distance and focus; art never arrives and is
+						never in focus.
+					</li>
+					<li>Art and artists do not exist.</li>
+					<li>Art and artists do exist.</li>
+					<li>To take advantage of topicality is iniquity.</li>
+					<li>Art is the friction between items 2 and 3.</li>
+					<li>
+						To make art you must be a butterfly catcher as well as a
+						caterpillar.
+					</li>
+					<li>Confidence is always meretricious but like fangs is useful.</li>
+					<li>
+						If humans make art, so do all lifeforms, whether tulips or trout or
+						whatever.
+					</li>
+					<li>“I am nature” is a really good artist’s statement.</li>
+					<li>If item 2 is true, then artists never die.</li>
+					<li>Art is not limited to “Art”.</li>
+					<li>Artists are not the only “Artists”.</li>
+					<li>
+						Cynicism seems like a bad mode but so do earthquakes. Still optimism
+						seems wise.
+					</li>
+					<li>No method or mode can be applied successfully to everything.</li>
+					<li>
+						A true “art” is unintelligible to everyone but the artist. Sorry.
+					</li>
+					<li>Anyone who gives more than he has is a thief.</li>
+					<li>Your mind doesn’t tell you the truth.</li>
+				</ul>
+				<ul className="flex justify-center col-start-1 col-end-3">
+					<li className="uppercase mr-8 text-[#949494] font-Neue">
+						<a
+							href="https://instagram.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Email
+						</a>
+					</li>
+					<li className="uppercase  text-[#949494] font-Neue">
+						<a
+							href="https://google.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							instagram
+						</a>
+					</li>
+				</ul>
+			</article>
+		</section>
+	);
+};
+export default SectionThree;
