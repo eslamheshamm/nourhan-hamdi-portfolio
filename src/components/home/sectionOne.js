cb514c6c2200063bb123474539a3d7d5f521a4bf
eslamheshamm <esl2mhesham@gmail.com>
2021-06-14 21:19:06 +0200

    building images grid

---
+++ src/components/home/sectionOne.js
@@ -0,0 +1,301 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+const SectionOne = () => {
+	return (
+		<section className="grid gap-5 py-6">
+			<div className="grid grid-cols-[2fr,2fr,1fr]  grid-rows-[400px]  w-10/12 mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+			<div className="grid grid-cols-[2fr,1fr,1.5fr,1fr,1.5fr,1.5fr] grid-rows-[200px] w-10/12   mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+			<div className="grid grid-cols-[1fr,2fr,2fr,2fr,1fr,1.5fr,1fr,,2fr,1fr] grid-rows-[150px]  w-10/12  mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+			<div className="grid grid-cols-[1fr,2fr,2fr,1fr,1fr,2fr] grid-rows-[200px]  w-10/12  mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+			<div className="grid grid-cols-[1fr,1fr,1fr,2fr] grid-rows-[400px]  w-10/12  mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+			<div className="grid grid-cols-[1fr,1fr,2fr,2fr,2fr] grid-rows-[230px]  w-10/12  mx-auto gap-2 md:gap-5">
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+				<figure className=" w-full h-full">
+					<StaticImage
+						src="https://source.unsplash.com/random"
+						alt="A dinosaur"
+						placeholder="blurred"
+						layout="fullWidth"
+						className=" w-full h-full"
+					/>
+				</figure>
+			</div>
+		</section>
+	);
+};
+export default SectionOne;
